9d68834c6c9b7959a2564b9bb2f596d22e7889c9
MK <quvondiq@mail.ru>
2020-01-06 16:30:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let a = 5;
+console.log(a)
+let persone= {
+    name: "Abdulla",
+    age: 24,
+    isMarried: false
+};
+
+console.log(persone["age"]);
+
+let arr = ['plum.png','hap.jpg','apple.bmp'];
+
+console.log(arr[2]);
+
+// alert('dsadsad');
+
+// let answer= confirm('here you are?');
+// console.log(answer);
+
+// let answer= +prompt('18?','yes');
+// console.log(typeof(answer));
+// console.log(4+'-object');
+// console.log('arr[2]'+ '-object');
+
+let incr=10,
+    decr=10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%3);
+console.log("2"==2);
+
+let isCHacked= true,
+    isClose = true;
+
+    console.log(isCHacked || isClose);
